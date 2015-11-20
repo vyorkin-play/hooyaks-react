235da001dc204cdaea036fec5690aa24c7725c4f
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-11-20 19:01:13 +0300

    Very naive

--- src/components/connect.js
+++ src/components/connect.js
@@ -1,3 +1,5 @@
+import isPlain from 'is-plain-obj';
+
 import { Component } from 'react';
 
 import { bindActionCreators } from 'hooyaks';
@@ -5,9 +7,26 @@ import { bindActionCreators } from 'hooyaks';
 import shallowEqual from '../utils/shallowEqual';
 import { storeShape } from './shapes/store';
 
+// simplified, naive implementation:
+// - no custom props merging func
+// - no versioning
+
+const stateIdentity = () => ({});
+const dispatchIdentity = (dispatch) => ({ dispatch });
+
+const wrapActions = (actions) =>
+  (dispatch) => bindActionCreators(mapDispatch, dispatch);
+
 export default (mapState, mapDispatch, options = {}) => {
   const { pure = true } = options;
 
+  //  app state to component props
+  const finalMapState = mapState || stateIdentity;
+  const finalMapDispatch = isPlain(mapDispatch) ?
+    wrapActions(mapDispatch) :
+    mapDispatch || dispatchIdentity;
+
+  // wrapper/decorator component
   return (Component) => {
     const name = Component.displayName || Component.name || 'Component';
 
@@ -17,13 +36,27 @@ export default (mapState, mapDispatch, options = {}) => {
       static contextTypes = { store: storeShape }
       static propTypes = { store: storeShape }
 
+      state = { store: null }
+
       constructor(props, context) {
         super(props, context);
         this.store = props.store || context.store;
+        this.updateState();
       }
 
+      updateState() {
+        this.nextState = this.computeState();
+      }
+
+      computeState = () => ({
+        ...finalMapState(this.store),
+        ...finalMapDispatch(this.store),
+        ...this.props
+      });
+
       shouldComponentUpdate(nextProps, nextState) {
-        return true; // TODO: does it matter right now?
+        // always update for now
+        return true;
       }
 
       render() {
